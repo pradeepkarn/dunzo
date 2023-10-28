b3337a766526c8fa0168733be7277d353a1f96a2
pradeepkarn <mail2pkarn@gmail.com>
2023-10-28 16:26:54 +0530

    order history

--- routes/api_routes.php
+++ routes/api_routes.php
@@ -5,7 +5,7 @@ $api_routes = [
     "/api/v1/account/login-via-token/{ug}" => 'Users_api@login_via_token@name.loginAccountViaTokenApi',
     "/api/v1/orders/list" => 'Orders_api@fetch_orders@name.fetchOrdersApi',
     "/api/v1/orders/driver/accept" => 'Orders_api@accept_order@name.acceptOrderApi',
-    "/api/v1/orders/driver/history" => 'Orders_api@aorder_history@name.orderHIstoryApi',
+    "/api/v1/orders/driver/history" => 'Orders_api@order_history@name.orderHIstoryApi',
     "/api/v1/account/update/location" => 'Orders_api@update_location@name.updateLocationApi',
 ];
 
