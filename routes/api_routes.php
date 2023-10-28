e72443a203a50b3bd662a0af7549ff474b998063
pradeepkarn <mail2pkarn@gmail.com>
2023-10-28 16:22:42 +0530

    we

--- routes/api_routes.php
+++ routes/api_routes.php
@@ -4,7 +4,7 @@ $api_routes = [
     "/api/v1/account/login/{ug}" => 'Users_api@login@name.loginAccountApi',
     "/api/v1/account/login-via-token/{ug}" => 'Users_api@login_via_token@name.loginAccountViaTokenApi',
     "/api/v1/orders/list" => 'Orders_api@fetch_orders@name.fetchOrdersApi',
-    "/api/v1/orders/driver/action" => 'Orders_api@accept_order@name.acceptOrderApi',
+    "/api/v1/orders/driver/accept" => 'Orders_api@accept_order@name.acceptOrderApi',
     "/api/v1/account/update/location" => 'Orders_api@update_location@name.updateLocationApi',
 ];
 
