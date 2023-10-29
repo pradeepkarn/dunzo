fdde8cc4c0d86bb4d2d4973e1bba92479840d10d
pradeepkarn <mail2pkarn@gmail.com>
2023-10-29 18:23:01 +0530

    fuels

---
+++ controllers/api/Fuels_api.api.php
@@ -0,0 +1,65 @@
+<?php
+class Fuels_api
+{
+    public $db;
+    function __construct()
+    {
+        $this->db = (new DB_ctrl)->db;
+    }
+    function get_fules($req = null)
+    {
+        header('Content-Type: application/json');
+        $req = obj($req);
+        $data  = json_decode(file_get_contents('php://input'));
+        $rules = [
+            'token' => 'required|string'
+        ];
+        $pass = validateData(data: arr($data), rules: $rules);
+        if (!$pass) {
+            $api['success'] = false;
+            $api['data'] = null;
+            $api['msg'] = msg_ssn(return: true, lnbrk: ", ");
+            echo json_encode($api);
+            exit;
+        }
+        $user = false;
+        $user = (new Users_api)->get_user_by_token($data->token);
+        if ($user) {
+            if ($user['user_group'] != 'driver') {
+                msg_set("Invalid login portal");
+                $api['success'] = false;
+                $api['data'] = null;
+                $api['msg'] = msg_ssn(return: true, lnbrk: ", ");
+                echo json_encode($api);
+                exit;
+            }
+            try {
+                $dt = $this->fuel_list($driver_id = $user['id']);
+                msg_set(count($dt) ? "Fuels found" : "Fuels not found");
+                $api['success'] = count($dt) ? true : false;
+                $api['data'] = count($dt) ? $dt : null;
+                $api['msg'] = msg_ssn(return: true, lnbrk: ", ");
+                echo json_encode($api);
+                exit;
+            } catch (PDOException $th) {
+                msg_set("Unable to fetch");
+                $api['success'] = false;
+                $api['data'] = null;
+                $api['msg'] = msg_ssn(return: true, lnbrk: ", ");
+                echo json_encode($api);
+                exit;
+            }
+        } else {
+            msg_set("User not found, invalid token");
+            $api['success'] = false;
+            $api['data'] = null;
+            $api['msg'] = msg_ssn(return: true, lnbrk: ", ");
+            echo json_encode($api);
+            exit;
+        }
+    }
+    function fuel_list($driverid)
+    {
+        return $this->db->filter("select id, volume, unit, fuel_group as fuel_type, balance from fuels where user_id = '$driverid'");
+    }
+}
--- routes/api_routes.php
+++ routes/api_routes.php
@@ -3,6 +3,9 @@ $api_routes = [
     "/api/v1/account/create/{ug}" => 'Users_api@create_account@name.createAccountApi',
     "/api/v1/account/login/{ug}" => 'Users_api@login@name.loginAccountApi',
     "/api/v1/account/login-via-token/{ug}" => 'Users_api@login_via_token@name.loginAccountViaTokenApi',
+
+    "/api/v1/fuels/show-fuels" => 'Fuels_api@get_fules@name.getFuelsApi',
+
     "/api/v1/orders/list" => 'Orders_api@fetch_orders@name.fetchOrdersApi',
     "/api/v1/orders/driver/accept" => 'Orders_api@accept_order@name.acceptOrderApi',
     "/api/v1/orders/driver/status-change/{delivery_status}" => 'Orders_api@status_update_order@name.statustOrderApi',
