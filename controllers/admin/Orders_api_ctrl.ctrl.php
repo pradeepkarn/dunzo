8c679508d205fde9246975daa247d5c15d575e94
pradeepkarn <mail2pkarn@gmail.com>
2023-10-29 13:41:49 +0530

    test

--- controllers/admin/Orders_api_ctrl.ctrl.php
+++ controllers/admin/Orders_api_ctrl.ctrl.php
@@ -188,7 +188,7 @@ class Orders_api_ctrl
         
         if ($driver_id!="0") {
             $ruuning = $this->db->showOne("select * from orders where driver_id = '{$driver_id}' and delivery_status IN (0,1,2)");
-            if ($ruuning['driver_id']) {
+            if ($ruuning) {
                 msg_set("Driver already assigned");
             }else{
                 $this->db->insertData['driver_id'] = $driver_id;
