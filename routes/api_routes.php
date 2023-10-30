fac1c77e26a81a23f274245fdb35c7e349df8299
pradeepkarn <mail2pkarn@gmail.com>
2023-10-30 16:49:05 +0530

    update profile api

--- routes/api_routes.php
+++ routes/api_routes.php
@@ -1,6 +1,7 @@
 <?php
 $api_routes = [
     "/api/v1/account/create/{ug}" => 'Users_api@create_account@name.createAccountApi',
+    "/api/v1/account/update/{ug}" => 'Users_api@update_account@name.createAccountApi',
     "/api/v1/account/login/{ug}" => 'Users_api@login@name.loginAccountApi',
     "/api/v1/account/login-via-token/{ug}" => 'Users_api@login_via_token@name.loginAccountViaTokenApi',
     "/api/v1/account/set-user-online/{ug}" => 'Users_api@set_user_online@name.setUserOnlineApi',
